fe5273bfb80a117da248613e5bfceccc1efa0aa9
Alexandra Ngaleba-Ze <89806302+ProcessOptimizingDataAnalyst@users.noreply.github.com>
2023-10-15 21:15:44 -0400

    Update Transformations_Script.py

--- Midterm_Exam_Project/Transformations_Script.py
+++ Midterm_Exam_Project/Transformations_Script.py
@@ -6,19 +6,31 @@ date_str = sys.argv[1]
 
 
 # creating spark session
-# change to yarn for EMR cluster
 spark = SparkSession.builder.master("yarn").appName("demo").getOrCreate()
 
 
 # Read files from s3 bucket
-# change to s3 bucket when running in EMR
 sales_df = spark.read.option("header","true").option("delimiter",",").csv("s3://de-midterm-raw/sales_2023-07-30.csv")
 inventory_df = spark.read.option("header","true").option("delimiter",",").csv("s3://de-midterm-raw/inventory_2023-07-30.csv")
 product_df = spark.read.option("header","true").option("delimiter",",").csv("s3://de-midterm-raw/product_2023-07-30.csv")
 store_df = spark.read.option("header","true").option("delimiter",",").csv("s3://de-midterm-raw/store_2023-07-30.csv")
 calendar_df = spark.read.option("header","true").option("delimiter",",").csv("s3://de-midterm-raw/calendar_2023-07-30.csv")
 
-# total sales quantity by week, by store and by product
+# Calculate the transformations metrics below by week, by store and by product:
+	- Total sales quantity
+	- Total sales amount
+	- Average sales price
+	- Stock level
+	- Inventory on order level
+	- Total cost
+	- Percentage of store in-stock
+	- Total low Stock impact
+	- Potential low stock impact
+	- No Stock impact
+	- Low Stock instances
+	- No Stock instances
+	- How many weeks the on hand stock can supply
+
 sales_df.createOrReplaceTempView("sales")
 inventory_df.createOrReplaceTempView("inventory")
 product_df.createOrReplaceTempView("product")
@@ -50,7 +62,7 @@ FROM
        inventory.inventory_on_hand_qty AS inventory_on_hand_qty,
        inventory.OUT_OF_STOCK_FLG  AS OUT_OF_STOCK_FLG,
        sum(sales.sales_qty) as sum_sales_qty,
-	   sum(sales.sales_amt) as sum_sales_amt,
+       sum(sales.sales_amt) as sum_sales_amt,
        sum(sales.sales_amt)/sum(sales.sales_qty) as avg_price,
        sum(inventory.inventory_on_hand_qty) as stock_level,
        sum(inventory.inventory_on_order_qty) as order_level,
@@ -74,12 +86,10 @@ ORDER BY 1,2,3,4,5,6
 GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13
 """)
 
-
+# Show transformations
 df_agg_transformations.show()
 
-
 # write the file back to s3
-# change this to s3 bucket for EMR
 df_agg_transformations.repartition(1).write.mode("overwrite").option("compression","gzip").parquet(f"s3://midterm-data-result-b7/date={date_str}")
 
 
